cd139872220f22f3e5dda338e8d0a619f142d23d
BenoiRa <becaremail@gmail.com>
2023-04-12 10:06:16 +0530

    sdflk

--- yolov5/detect.bat
+++ yolov5/detect.bat
@@ -1,9 +1,9 @@
 set imgSize=-1
 set source=--source VehicleR
 set weights=--weights ./main/weights/singleclass.pt 
-set output=--project ./main/output/
+set output=--project ./output/
 set name=--name detect --save-txt --max-det 40
-set line_thickness=--line-thickness 2 --update
+set line_thickness=--line-thickness 2
 @REM set codes = '#--visualize' 
 
 python ./main/yolov5/detect.py %source% %weights% %output% %line_thickness% %name%
--- yolov5/train.bat
+++ yolov5/train.bat
@@ -1,10 +1,12 @@
 set imgSize=--img 640 
 set batch=--batch -1 
 set epochs=--epochs 3
-set data=--data ./singlecls ds/vehicle.yaml
-set weights=@REM --weights ./main/yolov5/yolov5s.pt 
+set data=--data ./singlecls_ds/vehicle.yaml
+set weights= 
+@REM --weights ./main/yolov5/yolov5s.pt 
 set name=--name pretrained_5s 
-set code=--patience 30 --project ./main/train @REM --cfg ./main/yolov5/models/yolov5s.yaml 
+set code=--patience 30 --project ./main/train
+@REM  --cfg ./main/yolov5/models/yolov5s.yaml 
 
 
-python ./main/yolov5/train.py $imgSize $batch $epochs $data $weights $cfg $name $code
+python ./main/yolov5/train.py %imgSize% %batch% %epochs% %data% %weights% %name% %code%
